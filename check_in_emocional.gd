17e68c59ed7b6a6b9ed08abbc6d7c8849f2b86a7
Leo Maia <leomaia@edu.unifor.br>
2026-05-27 15:19:25 -0300

    correção design visual e bug ESP

--- check_in_emocional.gd
+++ check_in_emocional.gd
@@ -1,9 +1,19 @@
 extends Control
 
 func _ready():
-	# Caminho direto a partir da raiz onde o script está anexado
-	for botao in $MarginContainer/VBoxContainer/GridContainer.get_children():
-		botao.pressed.connect(_ao_selecionar_humor.bind(botao.name))
+	# Lista com os nomes exatos de todos os botões soltos na cena
+	var botoes = ["Feliz", "Triste", "Bravo", "Assustado", "Cansado", "Orgulhoso"]
+	
+	for nome_botao in botoes:
+		# Busca o botão diretamente como filho do nó principal (raiz)
+		if has_node(nome_botao):
+			var botao = get_node(nome_botao)
+			
+			# Garante que o Action Mode do botão reaja no clique imediato (Button Press)
+			botao.action_mode = BaseButton.ACTION_MODE_BUTTON_PRESS
+			
+			# Conecta o clique à função que gerencia a escolha da criança
+			botao.pressed.connect(_ao_selecionar_humor.bind(nome_botao))
 
 func _ao_selecionar_humor(nome_humor):
 	# Guarda no Global se é Bravo, Feliz, Triste, etc.
